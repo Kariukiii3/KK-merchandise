bf5bcaa01f777eab41f6dabbb4a0b9ad8c2cbb65
kariukiii3 <gachaukariuki9@gmail.com>
2023-10-19 15:44:21 +0300

    updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+// checks whether the passwords correspond for sign up
+const form = document.querySelector('form');
+    form.addEventListener('submit', function(event) {
+        const password = document.getElementById('password').value;
+        const confirmPassword = document.getElementById('confirmPassword').value;
+        if (password !== confirmPassword) {
+            event.preventDefault();
+            alert('Passwords do not match. Please re-enter your password.');
+        }
+    });
